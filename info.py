0db44e7583000171b41576ffa21fb3e23bad0b58
Sabit <sabittx410@gmail.com>
2025-09-19 15:37:47 +0600

    Update info.py

--- info.py
+++ info.py
@@ -25,8 +25,8 @@ MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/backup20020')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002838827489').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv:learningbajority&appName=learningbots")
-DATABASE_NAME = environ.get('DATABASE_NAME', "learningbots")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://sabit404:sabit404@sabit404.1bkne2q.mongodb.net/?retryWrites=true&w=majority&appName=SABIT404")
+DATABASE_NAME = environ.get('DATABASE_NAME', "sabit404")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
